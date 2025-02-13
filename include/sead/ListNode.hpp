761795998edbc052bcb70d2c8c899d31a9833f24
Mario Possamato <51346942+mariopossamato@users.noreply.github.com>
2025-02-13 01:58:47 -0500

    Update ListNode.hpp

--- include/sead/ListNode.hpp
+++ include/sead/ListNode.hpp
@@ -1,3 +1,14 @@
 #ifndef SEAD_LISTNODE_HPP
 #define SEAD_LISTNODE_HPP
+
+namespace sead {
+class ListNode {
+  void insertBack_(ListNode* pListNode);
+  void insertFront_(ListNode* pListNode);
+  void erase_();
+  ListNode* m_pPrev = nullptr;
+  ListNode* m_pNext = nullptr;
+};
+} // namespace sead
+
 #endif
