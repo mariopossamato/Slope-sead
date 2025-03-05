443e5faaa351114e29fce57f7ae656cbefe09276
Mario Possamato <51346942+mariopossamato@users.noreply.github.com>
2025-03-05 03:51:46 -0500

    Update ListNode.cpp

--- src/sead/ListNode.cpp
+++ src/sead/ListNode.cpp
@@ -2,21 +2,23 @@
 
 namespace sead {
 
-void ListNode::insertBack_(ListNode* pListNode) {
-  this->m_pNext = pListNode;
-  pListNode->m_pPrev = this;
-  pListNode->m_pNext = this->m_pNext;
-  if (this->m_pNext) {
-    this->m_pNext->m_pPrev = pListNode;
+void ListNode::insertBack_(ListNode* pNode) {
+  ListNode* pNext = this->m_pNext;
+  this->m_pNext = pNode;
+  pNode->m_pPrev = this;
+  pNode->m_pNext = pNext;
+  if (pNext) {
+    pNext->m_pPrev = pNode;
   }
 }
 
-void ListNode::insertFront_(ListNode* pListNode) {
-  this->m_pPrev = pListNode;
-  pListNode->m_pPrev = this->m_pPrev;
-  pListNode->m_pNext = this;
-  if (this->m_pPrev) {
-    this->m_pPrev->m_pNext = pListNode;
+void ListNode::insertFront_(ListNode* pNode) {
+  ListNode* pPrev = this->m_pPrev;
+  this->m_pPrev = pNode;
+  pNode->m_pPrev = pPrev;
+  pNode->m_pNext = this;
+  if (pPrev) {
+    pPrev->m_pNext = pNode;
   }
 }
 
